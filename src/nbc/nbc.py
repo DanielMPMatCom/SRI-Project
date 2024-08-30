69f66121044eb66871746a65fd0879a941f3199b
osvaldo.rmp <osvaldo.rmp@avangenio.com>
2024-08-30 17:00:57 -0400

    changes names

--- src/nbc/nbc.py
+++ src/nbc/nbc.py
@@ -6,40 +6,117 @@ def nbcf(rating: np.ndarray, alpha, r):
     users, movies = rating.shape
     r_alpha = r * alpha
 
-    pu = np.zeros((users, r + 1))
-    pi = np.zeros((movies, r + 1))
+    pu = np.zeros((users, r))
+    pi = np.zeros((movies, r))
 
     for movie in range(movies):
         movie_users = np.sum(rating[:, movie] != -1) + r_alpha
-        for qualified in range(r + 1):
+        for qualified in range(r):
             pi[movie, qualified] = (
-                np.sum(rating[:, movie] == qualified) + alpha
+                np.sum(rating[:, movie] == qualified + 1) + alpha
             ) / movie_users
 
     for user in range(users):
         user_movie = np.sum(rating[user, :] != -1) + r_alpha
-        for qualified in range(r + 1):
+        for qualified in range(r):
             pu[user, qualified] = (
-                np.sum(rating[user, :] == qualified) + alpha
+                np.sum(rating[user, :] == qualified + 1) + alpha
             ) / user_movie
 
-    item_likehood = np.zeros((movies, r + 1, movies, r + 1), 0)
+    item_likelihood = np.zeros((users, movies, r, movies))
 
-    for imovie in range(movie):
-        for ir in range(r + 1):
-            count_user_ir = np.sum(rating[:, imovie] == ir)
-            for jmovie in range(movie):
-                for jr in range(r + 1):
-                    count_user_jr = np.sum(rating[:, jmovie] == jr)
-                    
+    for user in range(users):
+        for movie in range(movies):
+            if rating[user, movie] == -1:
+                for qualified in range(r):
+                    collaborative_filtering = rating[:, movie] == qualified + 1
 
-    return pu, pi
+                    for j_movie in range(movies):
+                        if movie == j_movie or rating[user, j_movie] == -1:
+                            continue
+
+                        item_likelihood[user, movie, qualified, j_movie] = (
+                            np.sum(
+                                np.logical_and(
+                                    rating[:, j_movie] == rating[user, j_movie],
+                                    collaborative_filtering,
+                                )
+                            )
+                            + alpha
+                        ) / (
+                            np.sum(
+                                np.logical_and(
+                                    rating[:, j_movie] != -1, collaborative_filtering
+                                )
+                            )
+                            + r_alpha
+                        )
+
+        user_likelihood = np.zeros((movies, users, r, users))
+
+        for movie in range(movies):
+            for user in range(users):
+                if rating[user, movie] == -1:
+                    for qualified in range(r):
+                        collaborative_filtering = rating[user, :] == qualified + 1
+                        for j_user in range(users):
+                            if user == j_user or rating[j_user, movie] == -1:
+                                continue
+
+                            user_likelihood[movie, user, qualified, j_user] = (
+                                np.sum(
+                                    np.logical_and(
+                                        rating[j_user, :] == rating[j_user, movie],
+                                        collaborative_filtering,
+                                    )
+                                )
+                                + alpha
+                            ) / (
+                                np.sum(
+                                    np.logical_and(
+                                        rating[j_user, :] != -1, collaborative_filtering
+                                    )
+                                )
+                                + r_alpha
+                            )
+
+    return pu, pi, item_likelihood, user_likelihood
+
+
+def predict_item(rating: np.ndarray, pi: np.ndarray, item_likelihood, r):
+    users, movies = rating.shape
+    prediction = np.zeros((users, movies, r))
+    for user in range(users):
+        for movie in range(movies):
+            for qualified in range(r):
+                tmp = pi[movie, qualified]
+                for j_movie in range(movies):
+                    if movie == j_movie or rating[user, j_movie] == -1:
+                        continue
+                    tmp *= item_likelihood[user, movie, qualified, j_movie]  # review
+                prediction[user, movie, qualified] = tmp
+    return prediction
+
+
+def predict_user(rating: np.ndarray, pu: np.ndarray, user_likelihood, r):
+    users, movies = rating.shape
+    prediction = np.zeros((users, movies, r))
+    for user in range(users):
+        for movie in range(movies):
+            for qualified in range(r):
+                tmp = pu[user, qualified]
+                for j_user in range(users):
+                    if user == j_user or rating[j_user, movie] == -1:
+                        continue
+                    tmp *= user_likelihood[movie, user, qualified, j_user]  # review
+                prediction[user, movie, qualified] = tmp
+    return prediction
 
 
 rating = np.array(
     [
         [-1, 1, 2, 2, 5, -1, 4, 3, 5],
-        [1, 5, 3, -1, 2, 4, 4, 5, -1],
+        [1, 5, 3, -1, 2, 4, 4, 3, -1],
         [1, 1, 2, -1, 2, 4, 4, 5, -1],
         [3, 2, 2, 3, -1, 1, 3, 2, -1],
         [5, 1, 5, 5, 4, 4, 5, 2, -1],
@@ -49,14 +126,21 @@ rating = np.array(
 ALPHA = 0.01
 R = 5
 
-a, b = nbcf(rating=rating, alpha=ALPHA, r=R)
+a, b, c, d = nbcf(rating=rating, alpha=ALPHA, r=R)
 print(a)
 print(" - - - - - ")
 print(b)
 
 print(" =========================================")
 
-a, b = nbcf_paper(rating=rating, alpha=ALPHA, r=R)
-print(a)
-print(" - - - -- ")
-print(b)
+print(c[0, 0, 0])
+
+print(" =========================================")
+
+print(d[0, 0, 0])
+
+
+print(" =========================================")
+
+p = predict_item(rating, b, c, R)
+print(p[0, 0])
