da3deb3a9b848e9384cc3e6998f8209db8d3b172
Phann020126 <daniel020126@gmail.com>
2024-09-01 15:16:50 -0400

    hybrid approach group extension

---
+++ src/extended_naive_bayes/NBP.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def group_prediction(
+    rating: np.ndarray,
+    group: np.ndarray,
+    ip: np.ndarray,
+    up: np.ndarray,
+    hp: np.ndarray,
+    alpha: float,
+    r: int,
+):
+    
+    _, movies = rating.shape
+    group_prediction_hf = np.zeros(movies, r)
+
+    for item in range(movies):
+        for qualified in range(r):
+
+            hybrid_based = 1
+            inverted_hybrid_based = 1
+
+            for g_user in group:
+                hybrid_based *= hp[g_user, item, qualified]
+                inverted_hybrid_based *= 1 - hp[g_user, item, qualified]
+
+            group_prediction_hf[item, qualified] = (hybrid_based) / (hybrid_based + inverted_hybrid_based)
+
+    return group_prediction_hf
+                     
+
