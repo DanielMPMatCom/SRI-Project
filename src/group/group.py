9fa6607ee6e015e8ffc301eb3149ec2f99d798b9
osvaldo.rmp <osvaldo.rmp@avangenio.com>
2024-08-31 20:51:49 -0400

    test

--- src/group/group.py
+++ src/group/group.py
@@ -2,22 +2,67 @@ import numpy as np
 
 
 def unpopular(rating: np.ndarray, movie: int):  # pi
+    """
+    Calculate the unpopularity score of a movie based on the ratings.
+
+    Parameters:
+    rating (np.ndarray): The array of ratings for each movie by each user.
+    movie (int): The index of the movie for which the unpopularity score is calculated.
+
+    Returns:
+    float: The unpopularity score of the movie, ranging from 0 to 1.
+    """
     return 1 - np.sum(rating[:, movie] != -1) / rating.shape[0]
 
+
 def all_group_movies(rating: np.ndarray, group: np.ndarray):
+    """
+    Returns a boolean array indicating whether each movie in the rating matrix has been rated by any user in the given group.
+
+    Parameters:
+    rating (np.ndarray): The rating matrix where each row represents a user and each column represents a movie.
+    group (np.ndarray): The array of user IDs in the group.
+
+    Returns:
+    np.ndarray: A boolean array of shape (rating.shape[1],) indicating whether each movie has been rated by any user in the group.
+    """
     group_movies = np.full(rating.shape[1], False)
     for u in group:
         group_movies = np.logical_or(group_movies, rating[u, :] != -1)
     return group_movies
 
+
 def interception_movies_group_user(rating: np.ndarray, group: np.ndarray, user: int):
+    """
+    Returns a boolean array indicating whether each movie in the group is rated by the given user.
+
+    Parameters:
+    rating (np.ndarray): The rating matrix.
+    group (np.ndarray): The group of movies.
+    user (int): The user index.
+
+    Returns:
+    np.ndarray: A boolean array indicating whether each movie in the group is rated by the given user.
+    """
     group_movies = all_group_movies(rating=rating, group=group)
     user_movies = rating[user, :] != -1
     return np.logical_and(user_movies, group_movies)
 
+
 def user_group_similarity(
     rating: np.ndarray, group: np.ndarray, user: int
 ):  # Xgu Jacard index
+    """
+    Calculates the similarity between a user and a group based on their movie ratings.
+
+    Parameters:
+        rating (np.ndarray): 2D array representing the movie ratings of all users.
+        group (np.ndarray): 1D array representing the group of users.
+        user (int): Index of the user for which the similarity is calculated.
+
+    Returns:
+        float: The similarity between the user and the group, measured using the Jaccard index.
+    """
     group_movies = all_group_movies(rating=rating, group=group)
     user_movies = rating[user, :] != -1
 
@@ -34,13 +79,38 @@ def user_group_similarity(
         [unpopular(rating=rating, movie=i) for i in range(rating.shape[1]) if union[i]]
     )
 
+
 def user_singularity(rating: np.ndarray, user: int, movie: int):
+    """
+    Calculate the singularity of a user for a specific movie.
+
+    Parameters:
+    rating (np.ndarray): The rating matrix.
+    user (int): The index of the user.
+    movie (int): The index of the movie.
+
+    Returns:
+    float: The singularity of the user for the movie.
+    """
     movie_users = rating[:, movie] != -1
     return np.sum(
         np.logical_and(movie_users, rating[:, movie] != rating[user, movie])
     ) / np.sum(movie_users)
 
+
 def item_group_singularity(rating: np.ndarray, group: np.ndarray, movie: int):
+    """
+    Calculate the singularity of a movie within a group of users.
+
+    Parameters:
+    rating (np.ndarray): The rating matrix of shape (num_users, num_movies).
+    group (np.ndarray): The array of user indices representing the group.
+    movie (int): The index of the movie.
+
+    Returns:
+    float: The singularity of the movie within the group. If there are no users in the group who have rated the movie, returns 0.
+    """
+
     movie_users_in_group = [user for user in group if rating[user][movie] != -1]
 
     return (
@@ -57,16 +127,40 @@ def item_group_singularity(rating: np.ndarray, group: np.ndarray, movie: int):
         else 0
     )
 
+
 def normalized_rating(rating: np.ndarray):
+    """
+    Normalize the given rating array.
+
+    Parameters:
+    rating (np.ndarray): The input rating array.
+
+    Returns:
+    np.ndarray: The normalized rating array.
+    """
     rating_min = np.min(rating[rating != -1])
     rating_max = np.max(rating[rating != -1])
     normalized = (rating - rating_min) / (rating_max - rating_min)
     normalized[rating == -1] = -1  # Restaurar los valores -1
     return normalized
 
+
 def mean_square_difference_group_rating(
     rating_normalized: np.ndarray, group: np.ndarray, user: int, movie: int
 ):
+    """
+    Calculate the mean square difference of the group ratings for a specific movie.
+
+    Parameters:
+        rating_normalized (np.ndarray): The normalized ratings matrix.
+        group (np.ndarray): The group of users.
+        user (int): The user for whom the mean square difference is calculated.
+        movie (int): The movie for which the mean square difference is calculated.
+
+    Returns:
+        float: The mean square difference of the group ratings for the specified movie.
+        None: If the user's rating or the group's ratings for the movie are missing.
+    """
     group_movie = [u for u in group if rating_normalized[u, movie] != -1]
 
     if rating_normalized[user, movie] == -1 or len(group_movie) == 0:
@@ -79,13 +173,27 @@ def mean_square_difference_group_rating(
         ]
     ) / len(group_movie)
 
+
 def singularity_dot(rating: np.ndarray, group: np.ndarray, user: int, movie: int):
+    """
+    Calculates the singularity dot product for a given user and movie.
+
+    Parameters:
+    - rating (np.ndarray): The rating matrix.
+    - group (np.ndarray): The group matrix.
+    - user (int): The user index.
+    - movie (int): The movie index.
+
+    Returns:
+    - float: The singularity dot product.
+    """
     movie_group = [u for u in group if rating[u, movie] != -1]
     return (
         user_singularity(rating=rating, user=user, movie=movie)
         * item_group_singularity(rating=rating, group=group, movie=movie)
         * len(movie_group)
     )
+
 
 def smgu(
     rating: np.ndarray,
@@ -94,6 +202,19 @@ def smgu(
     user: int,
     alpha: float,
 ):
+    """
+    Calculate the SMGU (Similarity Measure Group User) value for a given user in a group.
+
+    Parameters:
+        rating (np.ndarray): The rating matrix of shape (num_users, num_movies).
+        normalized_rating (np.ndarray): The normalized rating matrix of shape (num_users, num_movies).
+        group (np.ndarray): The group matrix of shape (num_users, num_groups).
+        user (int): The index of the user for whom to calculate the SMGU value.
+        alpha (float): The weight parameter for balancing the user-group similarity and the mean square difference.
+
+    Returns:
+        float: The SMGU value for the given user in the group.
+    """
     interception = interception_movies_group_user(rating=rating, group=group, user=user)
 
     n = 0
@@ -111,87 +232,3 @@ def smgu(
     x = user_group_similarity(rating=rating, group=group, user=user)
 
     return np.power(x, alpha) * np.power(y, 1 - alpha)
-
-rating = np.array(
-    [
-        [-1, 1, -1, 1, 1, -1, 0, -1, 0, -1, -1, 0],
-        [0, 1, -1, 0, 1, -1, 1, -1, 0, -1, -1, -1],
-        [-1, 1, -1, 0, 0, -1, 0, 0, -1, -1, -1, 0],
-        [-1, 1, 1, 1, 1, 1, 1, -1, 1, 0, 1, 1],
-        [1, 0, -1, 1, -1, 1, -1, -1, 1, 0, -1, 1],
-        [0, 1, -1, 0, 1, -1, 0, 0, 0, -1, -1, 0],
-        [1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1],
-        [1, 1, -1, 0, 0, 0, 0, -1, 0, -1, 1, 0],
-        [-1, 1, -1, 1, 0, -1, 1, -1, -1, 1, 1, -1],
-    ]
-)
-
-groups = np.array([[0, 1, 2]])
-group = groups[0]
-
-# # Test 1 Unpopularity
-# print([unpopular(rating, i) for i in range(rating.shape[1])])
-
-# # Test 2 User group similarity
-# print(
-#     [
-#         user_group_similarity(rating=rating, group=group, user=u)
-#         for u in range(rating.shape[0])
-#         if u not in group
-#     ]
-# )
-
-# # Test 3 User singularity
-# for u in range(rating.shape[0]):
-#     print(
-#         [
-#             (i + 1, user_singularity(rating=rating, user=u, movie=i))
-#             for i in range(rating.shape[1])
-#             if rating[u, i] != -1
-#         ]
-#     )
-
-# # Test 4 Item group singularity
-# print(
-#     [
-#         item_group_singularity(rating=rating, group=group, movie=i)
-#         for i in range(rating.shape[1])
-#     ]
-# )
-
-# # Test 5 Normalized rating
-# print(normalized_rating(rating))
-
-# # Required for following tests
-nr = normalized_rating(rating)
-
-# # Test 6 Mean square difference group rating
-# for u in range(rating.shape[0]):
-#     if u not in group:
-#         print(
-#             [
-#                 mean_square_difference_group_rating(
-#                     rating_normalized=nr,
-#                     group=group,
-#                     user=u,
-#                     movie=i,
-#                 )
-#                 for i in range(rating.shape[1])
-#             ]
-#         )
-
-# # Test 7 Final similarity (SMGU)
-# for alpha in [0, 0.25, 0.5, 0.75, 1]:
-#     print(
-#         [
-#             smgu(
-#                 rating=rating,
-#                 normalized_rating=nr,
-#                 group=group,
-#                 user=u,
-#                 alpha=1 - alpha,
-#             )
-#             for u in range(rating.shape[0])
-#             if u not in group
-#         ]
-#     )
---
+++ src/tests/test_recommendation_group_of_users.py
@@ -0,0 +1,325 @@
+import sys
+
+sys.path.append("src")
+from group.group import *
+
+
+def attempt(value, expected, test_name=""):
+    try:
+        assert value == expected
+        return "\033[92m" + f"✅ Test {test_name} passed!" + "\033[0m"
+    except AssertionError as e:
+        raise AssertionError(
+            f"❌ Test failed! Expected {expected}, but got {value}"
+        ) from e
+
+
+def test_group_of_users():
+    rating = np.array(
+        [
+            [-1, 1, -1, 1, 1, -1, 0, -1, 0, -1, -1, 0],
+            [0, 1, -1, 0, 1, -1, 1, -1, 0, -1, -1, -1],
+            [-1, 1, -1, 0, 0, -1, 0, 0, -1, -1, -1, 0],
+            [-1, 1, 1, 1, 1, 1, 1, -1, 1, 0, 1, 1],
+            [1, 0, -1, 1, -1, 1, -1, -1, 1, 0, -1, 1],
+            [0, 1, -1, 0, 1, -1, 0, 0, 0, -1, -1, 0],
+            [1, -1, -1, -1, -1, 1, -1, 1, -1, 1, -1, -1],
+            [1, 1, -1, 0, 0, 0, 0, -1, 0, -1, 1, 0],
+            [-1, 1, -1, 1, 0, -1, 1, -1, -1, 1, 1, -1],
+        ]
+    )
+
+    groups = np.array([[0, 1, 2]])
+    group = groups[0]
+
+    attempt(
+        [unpopular(rating, i) for i in range(rating.shape[1])],
+        [
+            0.4444444444444444,
+            0.11111111111111116,
+            0.8888888888888888,
+            0.11111111111111116,
+            0.2222222222222222,
+            0.5555555555555556,
+            0.2222222222222222,
+            0.6666666666666667,
+            0.33333333333333337,
+            0.5555555555555556,
+            0.6666666666666667,
+            0.33333333333333337,
+        ],
+    )
+
+    # Test 2 User group similarity
+    attempt(
+        [
+            user_group_similarity(rating=rating, group=group, user=u)
+            for u in range(rating.shape[0])
+            if u not in group
+        ],
+        [0.2608695652173913, 0.375, 1.0, 0.3125, 0.484848484848485, 0.1818181818181818],
+        "user_group_similarity",
+    )
+
+    expected = [
+        [
+            (2, 0.125),
+            (3, 0.0),
+            (4, 0.5),
+            (5, 0.42857142857142855),
+            (6, 0.25),
+            (7, 0.5714285714285714),
+            (9, 0.6666666666666666),
+            (10, 0.5),
+            (11, 0.0),
+            (12, 0.6666666666666666),
+        ],
+        [
+            (1, 0.4),
+            (2, 0.875),
+            (4, 0.5),
+            (6, 0.25),
+            (9, 0.6666666666666666),
+            (10, 0.5),
+            (12, 0.6666666666666666),
+        ],
+        [
+            (1, 0.6),
+            (2, 0.125),
+            (4, 0.5),
+            (5, 0.42857142857142855),
+            (7, 0.42857142857142855),
+            (8, 0.3333333333333333),
+            (9, 0.3333333333333333),
+            (12, 0.3333333333333333),
+        ],
+        [(1, 0.4), (6, 0.25), (8, 0.6666666666666666), (10, 0.5)],
+        [
+            (1, 0.4),
+            (2, 0.125),
+            (4, 0.5),
+            (5, 0.5714285714285714),
+            (6, 0.75),
+            (7, 0.42857142857142855),
+            (9, 0.3333333333333333),
+            (11, 0.0),
+            (12, 0.3333333333333333),
+        ],
+        [
+            (2, 0.125),
+            (4, 0.5),
+            (5, 0.5714285714285714),
+            (7, 0.5714285714285714),
+            (10, 0.5),
+            (11, 0.0),
+        ],
+    ]
+
+    # Test 3 User singularity
+    cont = 0
+    for u in range(rating.shape[0]):
+        if u not in group:
+            attempt(
+                [
+                    (i + 1, user_singularity(rating=rating, user=u, movie=i))
+                    for i in range(rating.shape[1])
+                    if rating[u, i] != -1
+                ],
+                expected[cont],
+                "user_singularity",
+            )
+            cont += 1
+
+    # Test 4 Item group singularity
+    attempt(
+        [
+            item_group_singularity(rating=rating, group=group, movie=i)
+            for i in range(rating.shape[1])
+        ],
+        [
+            0.6,
+            0.12500000000000003,
+            0,
+            0.5,
+            0.4717038926992324,
+            0,
+            0.47170389269923235,
+            0.3333333333333333,
+            0.3333333333333333,
+            0,
+            0,
+            0.3333333333333333,
+        ],
+        "item_group_singularity",
+    )
+
+    # # Test 5 Normalized rating
+    # attempt(
+    #     normalized_rating(rating),
+    #     normalized_rating(rating),
+    #     "normalized_rating",
+    # )
+
+    # Required for following tests
+    nr = normalized_rating(rating)
+
+    # Test 6 Mean square difference group rating
+
+    expected = [
+        [
+            None,
+            0.0,
+            None,
+            0.6666666666666666,
+            0.3333333333333333,
+            None,
+            0.6666666666666666,
+            None,
+            1.0,
+            None,
+            None,
+            1.0,
+        ],
+        [
+            1.0,
+            1.0,
+            None,
+            0.6666666666666666,
+            None,
+            None,
+            None,
+            None,
+            1.0,
+            None,
+            None,
+            1.0,
+        ],
+        [
+            0.0,
+            0.0,
+            None,
+            0.3333333333333333,
+            0.3333333333333333,
+            None,
+            0.3333333333333333,
+            0.0,
+            0.0,
+            None,
+            None,
+            0.0,
+        ],
+        [1.0, None, None, None, None, None, None, 1.0, None, None, None, None],
+        [
+            1.0,
+            0.0,
+            None,
+            0.3333333333333333,
+            0.6666666666666666,
+            None,
+            0.3333333333333333,
+            None,
+            0.0,
+            None,
+            None,
+            0.0,
+        ],
+        [
+            None,
+            0.0,
+            None,
+            0.6666666666666666,
+            0.6666666666666666,
+            None,
+            0.6666666666666666,
+            None,
+            None,
+            None,
+            None,
+            None,
+        ],
+        [0.2608695652173913, 0.375, 1.0, 0.3125, 0.484848484848485, 0.1818181818181818],
+    ]
+    cont = 0
+    for u in range(rating.shape[0]):
+        if u not in group:
+            attempt(
+                [
+                    mean_square_difference_group_rating(
+                        rating_normalized=nr,
+                        group=group,
+                        user=u,
+                        movie=i,
+                    )
+                    for i in range(rating.shape[1])
+                ],
+                expected[cont],
+                "mean_square_difference_group_rating",
+            )
+            cont += 1
+
+    # Test 7 Final similarity (SMGU)
+    expected = [
+        [0.2608695652173913, 0.375, 1.0, 0.3125, 0.484848484848485, 0.1818181818181818],
+        [
+            0.2730372810649739,
+            0.27800793149820285,
+            0.9386673898125864,
+            0.0,
+            0.5059460006895177,
+            0.21359159945550824,
+        ],
+        [
+            0.28577253459685525,
+            0.2061024266024252,
+            0.881096468697574,
+            0.0,
+            0.5279615459532919,
+            0.2509175424687925,
+        ],
+        [
+            0.2991017974225909,
+            0.15279495812400107,
+            0.8270565224454389,
+            0.0,
+            0.5509350674291535,
+            0.29476633575045147,
+        ],
+        [
+            0.3130527758646582,
+            0.11327522733708817,
+            0.7763309871513349,
+            0.0,
+            0.5749082501361167,
+            0.34627787215218064,
+        ],
+    ]
+    cont = 0
+    for alpha in [0, 0.25, 0.5, 0.75, 1]:
+        attempt(
+            [
+                smgu(
+                    rating=rating,
+                    normalized_rating=nr,
+                    group=group,
+                    user=u,
+                    alpha=1 - alpha,
+                )
+                for u in range(rating.shape[0])
+                if u not in group
+            ],
+            expected[cont],
+            "smgu",
+        )
+        cont += 1
+
+
+if __name__ == "__main__":
+    try:
+        test_group_of_users()
+        print("\033[92m" + "✅ Test group_of_users passed!" + "\033[0m")
+    except Exception as e:
+        print(
+            "\033[91m"
+            + f"❌ Test group_of_user_passed! failed with error: {e}"
+            + "\033[0m"
+        )
