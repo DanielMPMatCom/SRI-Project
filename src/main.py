107c72488be8f57752cc539586b1db9ffbf92584
Osvaldo R.Moreno P. <osvaldo020213@gmail.com>
2024-09-08 11:24:19 -0400

    remove cuda

--- src/main.py
+++ src/main.py
@@ -1,69 +1,4 @@
-# import os
-# import numpy as np
-# import torch
-# from extended_naive_bayes.nbp import group_prediction
-# from nbcf.nbcf_opt import nbcf, predict_hybrid
-# from ml_processing.ml_procesing import MovieLensProcessing
-
-
-# def main():
-#     # Load data
-#     if torch.cuda.is_available():
-#         print(f"Usando GPU: {torch.cuda.get_device_name(torch.cuda.current_device())}")
-#     else:
-#         print("No se está utilizando GPU. Usando CPU.")
-
-#     time = os.times()
-#     ml_processing = MovieLensProcessing(rating_path="./datasets/ml-1m/ratings.dat")
-#     rating = ml_processing.numpy_user_movie_matrix()
-
-#     # Create recommenders
-#     alpha = 0.01
-#     r = 5
-
-#     pi, pu = nbcf(rating=rating, alpha=alpha, r=r)
-
-#     hybrid_prediction = predict_hybrid(
-#         rating=rating, r=r, predict_item=pi, predict_user=pu
-#     )
-
-#     print("group recommender")
-#     # Group recommender
-#     groups = []
-#     group_predict = np.ndarray((len(groups), rating.shape[1], r))
-
-#     for i in len(groups):
-#         prediction = group_prediction(
-#             rating=rating, group=groups[i], hp=hybrid_prediction, r=r
-#         )
-#         for j in range(rating.shape[1]):
-#             for y in range(r):
-#                 group_predict[j][y] = prediction[j][y]
-
-#     print("final prediction")
-#     prediction = np.full(rating.shape, -1)
-
-#     users, movies = rating.shape
-#     for user in users:
-#         for movie in movies:
-#             if rating[user][movie] == -1:
-#                 current_y = 0
-#                 current_trust = 0
-#                 for y in range(r):
-#                     if hybrid_prediction[user][movie][y] > current_trust:
-#                         current_trust = hybrid_prediction[user][movie][y]
-#                         current_y = y
-#                 prediction[user][movie] = current_y
-
-#     print("Time:", os.times() - time)
-
-
-# if __name__ == "__main__":
-#     main()
-
-
 import os
-import numpy as np
 import torch
 from extended_naive_bayes.nbp import group_prediction
 from nbcf.nbcf_opt import nbcf, predict_hybrid
@@ -89,7 +24,6 @@ def main():
 
     # Convertir rating a tensor de PyTorch y mover a GPU si está disponible
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    rating_tensor = torch.tensor(rating, device=device)
 
     # Create recommenders
     alpha = 0.01
@@ -98,10 +32,10 @@ def main():
     # Iniciar la medición del tiempo
     start_time.record()
 
-    pi, pu, user_map, movie_map = nbcf(rating=rating_tensor, alpha=alpha, r=r)
+    pi, pu, user_map, movie_map = nbcf(rating=rating, alpha=alpha, r=r)
 
     hybrid_prediction = predict_hybrid(
-        rating=rating_tensor,
+        rating=rating,
         r=r,
         predict_item=pi,
         predict_user=pu,
